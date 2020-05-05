2f3e8387662e0964704f82f7872432bcb33a7fbd
Yura Alekseev <yuraalekseev00@gmail.com>
2020-05-05 21:32:18 +0300

    added auth guard

--- js/main.js
+++ js/main.js
@@ -8,3 +8,77 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+/*-----------------------------------*/
+
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.querySelector('#logInForm');
+const logInInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+
+
+let login = localStorage.getItem('userName');
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle('is-open');
+}
+
+function authorized() {
+  function logOut() {
+    login = null; 
+    localStorage.removeItem('userName');
+
+    userName.style.display = '';
+    buttonOut.style.display = '';
+    buttonAuth.style.display = '';
+    buttonOut.removeEventListener('click', logOut);
+    checkAuth();
+  }
+
+  userName.textContent = login;
+
+  buttonAuth.style.display = 'none';
+  userName.style.display = 'flex';
+  buttonOut.style.display = 'flex';
+
+  buttonOut.addEventListener('click', logOut);
+}
+
+
+function notAuthorized() {
+   function logIn(event) {
+      event.preventDefault();
+       if (logInInput.value === '') {
+           alert('Введите Ваш логин');
+       } else {
+           login = logInInput.value;
+           localStorage.setItem('userName', login);
+           toggleModalAuth();
+           logInForm.reset();
+       }
+       buttonAuth.removeEventListener('click', toggleModalAuth);
+       closeAuth.removeEventListener('click', toggleModalAuth);
+       logInForm.removeEventListener('submit', logIn);
+       checkAuth();
+   }
+
+  buttonAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener('click', toggleModalAuth);
+
+  logInForm.addEventListener('submit', logIn)
+}
+
+function checkAuth() {
+  if (login) {
+    authorized();
+  } else {
+    notAuthorized();
+  }
+}
+
+checkAuth();
