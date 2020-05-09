71acb99b59cff2f40f0b92f62eec1a99f5d12010
Yura Alekseev <yuraalekseev00@gmail.com>
2020-05-09 14:05:17 +0300

    added search of foods and restaurants

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ const restaurantHeading = menu.querySelector('.section-heading');
 const modalBody = document.querySelector('.modal-body');
 const modalPrice = document.querySelector('.modal-pricetag');
 const buttonClearCart = document.querySelector('.clear-cart');
+const inputSearch = document.querySelector('.input-search');
 
 let login = localStorage.getItem('userName');
 
@@ -348,6 +349,60 @@ function init() {
     close.addEventListener("click", toggleModal);
 
     logo.addEventListener('click', returnMain);
+
+    inputSearch.addEventListener('keydown', function (event) {
+        if (event.keyCode === 13) {
+            const target = event.target;
+            const value = target.value.toLowerCase().trim();
+            target.value= '';
+            if (!value || value.length < 2) {
+                target.style.backgroundColor = 'tomato';
+                setTimeout(function () {
+                    target.style.backgroundColor = '';
+                },2000);
+                return;
+            }
+
+            const goods = [];
+
+            getData('./db/partners.json')
+                .then(function (data) {
+                    const products = data.map(function (item) {
+                        return item.products;
+                    });
+                    console.log(products);
+
+                    products.forEach(function (product) {
+                        getData(`./db/${product}`)
+                            .then(function (data) {
+                                goods.push(...data);
+                                const searchGoods = goods.filter(function (item) {
+                                    return  item.name.toLowerCase().includes(value);
+                                });
+
+                                cardsMenu.textContent = '';
+
+                                containerPromo.classList.add('hide');
+                                restaurants.classList.add('hide');
+                                menu.classList.remove('hide');
+
+                                restaurantHeading.textContent = '';
+
+                                const heading = `
+			                            <h2 class="section-title restaurant-title">Результат поиска</h2>`;
+
+                                restaurantHeading.insertAdjacentHTML("beforeend", heading);
+
+                                return searchGoods;
+                            })
+                            .then(function (data) {
+                                data.forEach(createCardGood);
+                            })
+                    })
+                });
+
+        }
+    });
 
     checkAuth();
 
